55cc05b5e5473059b000a0cc3cff2a159bfa7553
MinhTri <tri.hobk19@hcmut.edu.vn>
2023-04-11 23:49:03 +0700

    Add scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+    public GameObject chooseState;
+    public GameObject pausePanel;
+
+    public void openChooseState() {
+        chooseState.SetActive(true);
+    }
+
+    public void chooseStateVSMachine() {
+        changeScence();
+    }
+
+    public void chooseStateVSPlayer() {
+        changeScence();
+    }
+
+    private void changeScence() {
+        SceneManager.LoadScene("Game"); 
+    }
+
+    public void pauseGame() {
+        pausePanel.SetActive(true);
+    }
+
+    public void resumeGame() {
+        pausePanel.SetActive(false);
+
+    }
+}
